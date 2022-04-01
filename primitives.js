52cae15291ef327c6aa785a5cea0ff47fa1e9115
Styve NEMATCHA <styvechristopher.nematcha@ynov.com>
2022-04-01 10:00:37 +0200

    how

--- primitives.js
+++ primitives.js
@@ -1,11 +1,4 @@
-console.log(typeof 42);
-// expected output: "number"
-
 console.log(typeof 'str');
-// expected output: "string"
-
+console.log(typeof 42);
 console.log(typeof true);
-// expected output: "boolean"
-
 console.log(typeof undeclaredVariable);
-// expected output: "undefined"
