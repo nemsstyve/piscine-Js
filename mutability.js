d27a7b52712a63321ee88eb0173de5f1d16afa1a
Styve NEMATCHA <styvechristopher.nematcha@ynov.com>
2022-04-01 13:41:56 +0200

    how

---
+++ mutability.js
@@ -0,0 +1,22 @@
+/*const person = {
+name: 'Rick',
+    age: 77,
+    country: 'US',
+}*/
+
+const clone1 = {
+    name: person.name,
+    age: person.age,
+    country: person.country,
+}
+
+const clone2 = {
+    name: person.name,
+    age: person.age,
+    country: person.country,
+}
+
+Object.freeze(clone1)
+Object.freeze(clone2)
+person.country = 'FR'
+person.age = 78
