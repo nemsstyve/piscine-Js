83f9701e0a102210923c45e1a091dd0a5782cdb9
Styve NEMATCHA <styvechristopher.nematcha@ynov.com>
2022-04-08 09:53:45 +0200

    how

---
+++ method-man.js
@@ -0,0 +1,19 @@
+function words(X) {
+    return X.split(" ");
+}
+
+function sentence(X) {
+    return X.toString();
+}
+
+function yell(X) {
+    return X.toUpperCase();
+}
+
+function whisper(X) {
+    return "*" + X.toLowerCase() + "*"
+}
+
+function capitalize(X) {
+    return X[0].toUpperCase() + X.slice(1).toLowerCase();
+}
