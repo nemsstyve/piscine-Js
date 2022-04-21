c4af8cb31655d8dffe8d0f83f1d1aed53522a3b6
Styve NEMATCHA <styvechristopher.nematcha@ynov.com>
2022-04-21 11:23:28 +0200

    how

---
+++ slicer.js
@@ -0,0 +1,36 @@
+function slice(s, min, max) {
+    var finalMin = 0
+    var finalMax = 0
+
+    if (min > 0 && min < s.length) {
+        finalMin = min
+    } else if (min > 0 && min > -s.length) {
+        finalMin = s.length + min
+    } else {
+        finalMin = 0
+    }
+    if (max > 0 && max < s.length) {
+        finalMax = max
+    } else if (max > 0 && max > -s.length) {
+        finalMax = s.length + max
+    } else {
+        finalMin = s.length
+    }
+
+    let result;
+    let str = typeof s === 'string';
+    if (str) {
+        result = ""
+    } else {
+        result = []
+    }
+
+    for (finalMin; finalMin < finalMax; finalMin++) {
+        if (str) {
+            result += s[finalMin]
+        } else {
+            result = [...result, s[finalMin]]
+        }
+    }
+    return result
+}
